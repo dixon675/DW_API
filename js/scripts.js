5d58630422b78bc49e3a0bbe05e7c6ee52a3596b
Josh Dixon <jdixon675@gmail.com>
2021-03-22 11:07:38 -0600

    jeopardy API switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,50 @@
 var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'get_tweets.php', true);
+var baseUrl = 'http://jservice.io/api/clues'
+var query;
 
+
+var val = '/value';
+var cat = '/category';
+
+var random = '/random';
+
+var answer = '/answer';
+
+// $("#submit").on("click",function() {
+
+// 	query = "&count=" + $("#num").val()
+
+// 	callAPI()
+
+// })
+
+
+
+
+xhr.open('GET', baseUrl, true);
 xhr.send(null);
 
 xhr.onload = function() {
 
 	if (xhr.status == 200) {
-		
+
+		var clues = JSON.parse(xhr.responseText);
 		// console.log(xhr.responseText);
 
-		var tweets = JSON.parse(xhr.responseText);
+		console.log(clues);
+		console.log(clues.question_type)
 
-		console.log(tweets);
+		clues.forEach(function(data) {
+			var selection = $("<select name='value' id='valSel'>");
 
+			var option = "<option value='" + data.value + "'>" + data.value + "</option>";
+
+			selection.html(option);
+
+		})
 		
 
 	}
 }
+
